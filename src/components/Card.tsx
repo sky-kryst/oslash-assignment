fc41ecf39d3365b8ebae961206d6ef48ff252ffb
sky-kryst <aakashmeshram@outlook.com>
2022-11-21 21:00:58 +0530

    fix(components): :bug: Card: make corners rounded

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,7 +7,8 @@ export const Card = ({ children, className }: IProps) => {
   return (
     <div
       className={
-        "rounded m-4 shadow-gray-300 shadow drop-shadow flex flex-col items-stretch h-fit w-fit overflow-hidden" +
+        "rounded-md m-4 shadow-gray-300 shadow drop-shadow flex flex-col items-stretch h-fit w-fit overflow-hidden" +
+          " " +
           className ?? ""
       }
     >
