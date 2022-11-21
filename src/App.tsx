07a71469e47ef14be69cc55d6f0542bd84faa47c
sky-kryst <aakashmeshram@outlook.com>
2022-11-21 14:59:30 +0530

    style(App): :lipstick: margin to button

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { Button } from "./components";
 function App() {
   return (
     <div className="h-full w-full">
-      <Button label="Share" />
+      <div className="mt-12 ml-10">
+        <Button label="Share" />
+      </div>
     </div>
   );
 }
