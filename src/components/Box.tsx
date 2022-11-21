54639d4a67f311eb68a2ecf70be9013d6e2388b6
sky-kryst <aakashmeshram@outlook.com>
2022-11-21 19:47:24 +0530

    style(components): :alien: add space before dynamic classes in Box

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -8,9 +8,11 @@ export const Box = ({ children, className, passive }: IProps) => {
   return (
     <div
       className={
-        `w-full border border-zinc-300 flex justify-between items-center px-2 ${
+        `w-full flex justify-between items-center px-2 ${
           !!passive ? "bg-slate-200" : "bg-white"
-        }` + className ?? ""
+        }` +
+          " " +
+          className ?? ""
       }
     >
       {children}
