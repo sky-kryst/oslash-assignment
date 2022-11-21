ea35676dbc8487a0b3894153d6bb7c9b69465281
sky-kryst <aakashmeshram@outlook.com>
2022-11-21 17:16:50 +0530

    feat(components): :sparkles: add popover component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { Button } from "./components";
+import { Button, Popover } from "./components";
 
 function App() {
   return (
     <div className="h-full w-full">
       <div className="mt-12 ml-10">
-        <Button label="Share" />
+        <Popover>
+          <Popover.Target>
+            <Button label="Share" />
+          </Popover.Target>
+          <Popover.Content>
+            <h1>Hi, I'm here</h1>
+          </Popover.Content>
+        </Popover>
       </div>
     </div>
   );
 }
 
-export default App
+export default App;
---
+++ src/components/Popover.tsx
@@ -0,0 +1,51 @@
+import React, { cloneElement, useContext, useState } from "react";
+
+interface IParentProps {
+  children: React.ReactNode[] | React.ReactNode;
+}
+interface IChildProps {
+  children: React.ReactElement<any, string | React.JSXElementConstructor<any>>;
+}
+
+const PopoverContext = React.createContext<{
+  isVisible: boolean;
+  setIsVisible: React.Dispatch<React.SetStateAction<boolean>>;
+}>({
+  isVisible: false,
+  setIsVisible: () => {},
+});
+
+export const Popover = ({ children }: IParentProps) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <PopoverContext.Provider
+      value={{
+        isVisible,
+        setIsVisible,
+      }}
+    >
+      <>{children}</>
+    </PopoverContext.Provider>
+  );
+};
+
+Popover.Target = ({ children }: IChildProps) => {
+  const { setIsVisible } = useContext(PopoverContext);
+
+  return cloneElement(children, { onClick: () => setIsVisible(true) });
+};
+
+Popover.Content = ({ children }: IChildProps) => {
+  const { isVisible, setIsVisible } = useContext(PopoverContext);
+
+  return isVisible ? (
+    <>
+      <div
+        className="fixed top-0 left-0 h-screen w-screen"
+        onClick={() => setIsVisible(false)}
+      ></div>
+      <div className="z-20 h-fit w-fit">{children}</div>
+    </>
+  ) : null;
+};
