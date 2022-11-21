c863555c85c45031dc56bbe1a4a064d0c9c91ded
sky-kryst <aakashmeshram@outlook.com>
2022-11-21 19:45:57 +0530

    style(assets): :lipstick: change svg dimensions to w/h-4

--- src/assets/SVGs/index.tsx
+++ src/assets/SVGs/index.tsx
@@ -6,7 +6,7 @@ export const ShareSVG = (props: IProps) => {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 20 20"
       fill="currentColor"
-      className="w-5 h-5"
+      className="w-4 h-4"
       {...props}
     >
       <path d="M13 4.5a2.5 2.5 0 11.702 1.737L6.97 9.604a2.518 2.518 0 010 .792l6.733 3.367a2.5 2.5 0 11-.671 1.341l-6.733-3.367a2.5 2.5 0 110-3.475l6.733-3.366A2.52 2.52 0 0113 4.5z" />
@@ -20,7 +20,7 @@ export const GlobeSVG = (props: IProps) => {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 20 20"
       fill="currentColor"
-      className="w-5 h-5"
+      className="w-4 h-4"
       {...props}
     >
       <path
