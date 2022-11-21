f57e2821672fbd3f25e1859ac444845e60139d63
sky-kryst <aakashmeshram@outlook.com>
2022-11-21 18:17:12 +0530

    style(components): :lipstick: add bg color to Box

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,14 +1,16 @@
 interface IProps {
   className?: string;
   children: React.ReactNode | React.ReactNode[];
+  passive?: boolean;
 }
 
-export const Box = ({ children, className }: IProps) => {
+export const Box = ({ children, className, passive }: IProps) => {
   return (
     <div
       className={
-        "w-full border border-zinc-300 flex justify-between items-center px-2" +
-          className ?? ""
+        `w-full border border-zinc-300 flex justify-between items-center px-2 ${
+          !!passive ? "bg-slate-200" : "bg-white"
+        }` + className ?? ""
       }
     >
       {children}
