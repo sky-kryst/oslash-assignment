cb4d6ee1aed4ef7a9441727e3a27ed8cfd32125a
sky-kryst <aakashmeshram@outlook.com>
2022-11-22 01:35:06 +0530

    style(components): :lipstick: modify styles: Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,7 +7,7 @@ export const Card = ({ children, className }: IProps) => {
   return (
     <div
       className={
-        "rounded-md mt-2 mx-auto shadow-gray-300 shadow drop-shadow flex flex-col items-stretch h-fit w-fit overflow-hidden" +
+        "rounded-md mt-2 shadow-gray-300 shadow drop-shadow flex flex-col items-stretch h-max w-max overflow-hidden" +
           " " +
           className ?? ""
       }
