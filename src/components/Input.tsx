743de48bdfe928aae0024ad70c1972ee4fb15c79
sky-kryst <aakashmeshram@outlook.com>
2022-11-23 15:34:38 +0530

    refactor(components): :recycle: update prop interface fields and style

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 
-interface IProps {
-  title: string;
-  placeholder: string;
+interface IProps
+  extends React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
   leftButton?: React.ReactNode;
   rightButton?: React.ReactNode;
+  containerClasses?:string;
+  inputClasses?:string;
 }
 
-export const Input = ({ leftButton, rightButton, ...props }: IProps) => {
+export const Input = ({ leftButton, rightButton,inputClasses,containerClasses, ...props }: IProps) => {
   return (
-    <div className="border border-slate-300 hover:border-blue-300 rounded-md flex items-stretch overflow-hidden h-10">
+    <div className={"border border-slate-300 hover:border-blue-300 rounded-md flex items-stretch overflow-hidden h-10"+" "+containerClasses??""}>
       {leftButton ?? null}
       <input
         {...props}
-        className="focus:outline-0 active:outline-0 h-full w-full flex items-center pl-2"
+        className={"focus:outline-0 active:outline-0 h-full w-full flex items-center pl-2"+" "+inputClasses??""}
       />
       {rightButton ?? null}
     </div>
