996fe708894bbe29a50ff43b3ffb74b80c71ccb0
sky-kryst <aakashmeshram@outlook.com>
2022-11-23 15:33:11 +0530

    refactor(components): :recycle: update prop interface names

--- src/components/Popover.tsx
+++ src/components/Popover.tsx
@@ -1,28 +1,38 @@
 import React, { cloneElement, useContext, useState } from "react";
 
-interface IParentProps {
+interface IPopoverProps {
   children: React.ReactNode[] | React.ReactNode;
 }
-interface IChildProps {
+interface ITargetProps {
   children: React.ReactElement<any, string | React.JSXElementConstructor<any>>;
+  triggerEvent?: string;
 }
 
 const PopoverContext = React.createContext<{
   isVisible: boolean;
   setIsVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  popoverCount: number;
+  setPopoverCount: React.Dispatch<React.SetStateAction<number>>;
 }>({
   isVisible: false,
   setIsVisible: () => {},
+  popoverCount: 0,
+  setPopoverCount: () => {},
 });
 
-export const Popover = ({ children }: IParentProps) => {
+export const Popover = ({ children }: IPopoverProps) => {
   const [isVisible, setIsVisible] = useState(false);
+  const [popoverCount, setPopoverCount] = useState(0);
+
+  const { popoverCount: previousPopoverCount = 0 } = useContext(PopoverContext);
 
   return (
     <PopoverContext.Provider
       value={{
         isVisible,
         setIsVisible,
+        popoverCount: popoverCount + previousPopoverCount,
+        setPopoverCount,
       }}
     >
       <>{children}</>
@@ -30,22 +40,32 @@ export const Popover = ({ children }: IParentProps) => {
   );
 };
 
-Popover.Target = ({ children }: IChildProps) => {
-  const { setIsVisible } = useContext(PopoverContext);
+Popover.Target = ({ children, triggerEvent }: ITargetProps) => {
+  const { setIsVisible, setPopoverCount } = useContext(PopoverContext);
 
-  return cloneElement(children, { onClick: () => setIsVisible(true) });
+  return cloneElement(children, {
+    [triggerEvent ?? "onClick"]: (event: any) => {
+      event?.preventDefault();
+      setPopoverCount((prevState) => prevState + 1);
+      setIsVisible(true);
+    },
+  });
 };
 
-Popover.Content = ({ children }: IChildProps) => {
-  const { isVisible, setIsVisible } = useContext(PopoverContext);
+Popover.Content = ({ children }: IPopoverProps) => {
+  const { isVisible, setIsVisible, popoverCount, setPopoverCount } =
+    useContext(PopoverContext);
 
   return isVisible ? (
     <>
       <div
         className="fixed top-0 left-0 h-screen w-screen"
-        onClick={() => setIsVisible(false)}
+        onClick={() => {
+          setPopoverCount((prevState) => prevState - 1);
+          setIsVisible(false);
+        }}
       ></div>
-      <div className="z-20">{children}</div>
+      <div className={`z-${popoverCount * 10} absolute top-0`}>{children}</div>
     </>
   ) : null;
 };
