1425ae62360e61139ea6f161129d175995f50814
sky-kryst <aakashmeshram@outlook.com>
2022-11-28 16:47:28 +0530

    refactor(components): :recycle: change keypress to keyup event

--- src/hooks/useKeyPress.ts
+++ src/hooks/useKeyPress.ts
@@ -6,7 +6,7 @@ type TKeyActionConfig = {
 
 export const useKeyPress = (keyActionConfig: TKeyActionConfig) => {
   useEffect(() => {
-    const keypressHandler = (event: KeyboardEvent) => {
+    const keyupHandler = (event: KeyboardEvent) => {
       const name = event.key;
       for (let key in keyActionConfig) {
         if (name === key) {
@@ -20,10 +20,10 @@ export const useKeyPress = (keyActionConfig: TKeyActionConfig) => {
       }
     };
 
-    document.addEventListener("keypress", keypressHandler);
+    document.addEventListener("keyup", keyupHandler);
 
     return () => {
-      document.removeEventListener("keypress", keypressHandler);
+      document.removeEventListener("keyup", keyupHandler);
     };
   }, []);
 };
