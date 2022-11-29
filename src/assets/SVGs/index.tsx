c44542a0d02c06ce71414ead2df5b5e79f13c794
sky-kryst <aakashmeshram@outlook.com>
2022-11-29 20:19:38 +0530

    feat(assets): :sparkles: add Close SVG

--- src/assets/SVGs/index.tsx
+++ src/assets/SVGs/index.tsx
@@ -85,3 +85,17 @@ export const EmptyBoxSVG = (props: IProps) => {
     </svg>
   );
 };
+
+export const CloseSVG = (props: IProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      className="w-4 h-4"
+      {...props}
+    >
+      <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+    </svg>
+  );
+};
