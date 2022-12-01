a81237169c8ace297666a899eb60ee9f35392d44
sky-kryst <aakashmeshram@outlook.com>
2022-12-01 13:57:42 +0530

    feat(utils): :sparkles: add NameToAvatar RFC

---
+++ src/utils/NameToAvatar.tsx
@@ -0,0 +1,23 @@
+export const NameToAvatar = ({
+    inputString,
+    corners,
+    className,
+  }: {
+    inputString: string;
+    corners: "rounded" | "circle";
+    className?: string;
+  }) => {
+    return (
+      <div
+        className={
+          (corners === "rounded" ? "rounded-md" : "rounded-full") +
+            " " +
+            "flex justify-center items-center h-full w-full bg-slate-400 text-white text-center" +
+            " " +
+            className ?? ""
+        }
+      >
+        {inputString[0].toUpperCase()}
+      </div>
+    );
+  };
