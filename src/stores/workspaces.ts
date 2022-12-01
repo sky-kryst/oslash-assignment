516a8b4b9c0ee24ae8b1fad29e3e7bda16489b3a
sky-kryst <aakashmeshram@outlook.com>
2022-12-01 14:10:21 +0530

    refactor(stores): :recycle: add id property to workspace

--- src/stores/workspaces.ts
+++ src/stores/workspaces.ts
@@ -1,9 +1,14 @@
+import { v4 as uuid } from "uuid";
 import create from "zustand";
 
 interface IWorkspacesStore {
-  workspaces: Array<{ name: string; noOfMembers: number }>;
+  workspaces: Array<{
+    id: string;
+    name: string;
+    noOfMembers: number;
+  }>;
 }
 
 export const useWorkspacesStore = create<IWorkspacesStore>((set) => ({
-  workspaces: [{ name: "OSlash", noOfMembers: 25 }],
+  workspaces: [{ id: uuid(), name: "OSlash", noOfMembers: 25 }],
 }));
