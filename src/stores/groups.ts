a613117e0d99dee7997ebc63c46b3176f7c10738
sky-kryst <aakashmeshram@outlook.com>
2022-12-01 14:11:25 +0530

    refactor(stores): :recycle: add id property to group

--- src/stores/groups.ts
+++ src/stores/groups.ts
@@ -1,9 +1,14 @@
 import create from "zustand";
+import { v4 as uuid } from "uuid";
+
 
 interface IGroupsStore {
-  groups: Array<{ name: string }>;
+  groups: Array<{
+    id:string;
+    name: string;
+  }>;
 }
 
 export const useGroupsStore = create<IGroupsStore>((set) => ({
-  groups: [{ name: "Engineering" }, { name: "Product" }],
+  groups: [{id:uuid(), name: "Engineering" }, { id:uuid(),name: "Product" }],
 }));
