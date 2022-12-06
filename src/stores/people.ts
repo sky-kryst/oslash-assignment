8ef7fe98aca2c8006810bc187de8dc0b214d87df
sky-kryst <aakashmeshram@outlook.com>
2022-12-06 20:56:28 +0530

    refactor(stores): :recycle: extract out type of a person, export it and add more person to store- people

--- src/stores/people.ts
+++ src/stores/people.ts
@@ -2,17 +2,22 @@ import create from "zustand";
 import { v4 as uuid } from "uuid";
 
 
+export type TPerson = {
+  id: string;
+  firstName: string;
+  lastName?: string;
+};
+
 interface IPeopleStore {
-  people: Array<{
-    id: string;
-    firstName: string;
-    lastName?: string;
-  }>;
+  people: Array<TPerson>;
 }
 
 export const usePeopleStore = create<IPeopleStore>((set) => ({
   people: [
     { id: uuid(), firstName: "Wade", lastName: "Cooper" },
     { id: uuid(), firstName: "Arlene", lastName: "Mccoy" },
+    { id: uuid(), firstName: "gfsdfe", lastName: "Mccoy" },
+    { id: uuid(), firstName: "fsag", lastName: "Mccoy" },
   ],
 }));
+
