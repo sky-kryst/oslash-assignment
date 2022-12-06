55a702a5253af5e7538819ff2877b571a75b2f55
sky-kryst <aakashmeshram@outlook.com>
2022-12-06 20:55:19 +0530

    refactor(stores): :recycle: extract out type of a group and export it- groups

--- src/stores/groups.ts
+++ src/stores/groups.ts
@@ -2,11 +2,13 @@ import create from "zustand";
 import { v4 as uuid } from "uuid";
 
 
+export type TGroup = {
+  id: string;
+  name: string;
+};
+
 interface IGroupsStore {
-  groups: Array<{
-    id:string;
-    name: string;
-  }>;
+  groups: Array<TGroup>;
 }
 
 export const useGroupsStore = create<IGroupsStore>((set) => ({
